2e93fe145c5db612955c895a5b9a789ea5ed9fcf
Ruan Anjos <anjosruanp@gmail.com>
2025-10-26 11:46:13 -0300

    Initializing React Studies: Chronos-Timer

---
+++ src/App.tsx
@@ -0,0 +1,20 @@
+import "./styles/theme.css";
+import "./styles/global.css";
+import { Heading } from "./components/Heading";
+
+export function App() {
+  return (
+    <>
+    <Heading>Salve cachorro</Heading>
+    <Heading>Salve cachorro 2</Heading>
+
+      <Heading />
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Repudiandae,
+        assumenda exercitationem fugit qui quidem ea, enim nesciunt soluta nisi,
+        necessitatibus aperiam perspiciatis unde consequatur consectetur
+        molestiae! Delectus excepturi odio libero!
+      </p>
+    </>
+  );
+}
