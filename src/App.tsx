605568e09bea2b8d8b12ddc42bb3095196f75da8
Ruan Anjos <anjosruanp@gmail.com>
2025-10-26 12:31:24 -0300

    Add TimerIcon to Heading component and update styles; include lucide-react dependency

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,17 @@
 import "./styles/theme.css";
 import "./styles/global.css";
 import { Heading } from "./components/Heading";
+import { TimerIcon } from "lucide-react";
 
 export function App() {
   return (
     <>
-    <Heading>Ola mundo 1!</Heading>
-
-      <Heading />
+      <Heading>
+        Ola mundo 1!
+        <button>
+          <TimerIcon />
+        </button>
+      </Heading>
       <p>
         Lorem ipsum dolor sit amet consectetur adipisicing elit. Repudiandae,
         assumenda exercitationem fugit qui quidem ea, enim nesciunt soluta nisi,
