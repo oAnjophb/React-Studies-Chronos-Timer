32a11cbb185adb0e3f7f36eeeb1a1186c06717bd
Ruan Anjos <anjosruanp@gmail.com>
2025-10-26 13:34:33 -0300

    Refactor App component structure: implement Container component, remove old layout, and update styles; add Logo component with TimerIcon

--- src/App.tsx
+++ src/App.tsx
@@ -1,29 +1,19 @@
+import { Container } from "./components/Container";
+import { Heading } from "./components/Heading";
+import { Logo } from "./components/Logo";
+
 import "./styles/theme.css";
 import "./styles/global.css";
 
 export function App() {
   return (
     <>
-      <div className="container">
-        <div className="content">
-          <section>LOGO</section>
-        </div>
-      </div>
-      <div className="container">
-        <div className="content">
-          <section>MENU</section>
-        </div>
-      </div>
-      <div className="container">
-        <div className="content">
-          <section>FORM</section>
-        </div>
-      </div>
-      <div className="container">
-        <div className="content">
-          <section>FOOTER</section>
-        </div>
-      </div>
+      <Container>
+        <Logo />
+      </Container>
+      <Container>
+        <Heading>MENU</Heading>
+      </Container>
     </>
   );
 }
