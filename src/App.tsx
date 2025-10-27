88d41ee8290a7a5001ca5ac027c70acfaab2e3d2
Ruan Anjos <anjosruanp@gmail.com>
2025-10-26 23:06:12 -0300

    Add Cycles component to display work and break cycles in the App layout

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import { Menu } from "./components/Menu";
 import { Logo } from "./components/Logo";
 import { CountDown } from "./components/CountDown";
 import { DefaultInput } from "./components/DefaultInput";
+import { Cycles } from "./components/Cycles";
 
 import "./styles/theme.css";
 import "./styles/global.css";
@@ -37,8 +38,7 @@ export function App() {
           </div>
 
           <div className="formRow">
-            <p>Ciclos</p>
-            <p>0 0 0 0 0 0 0</p>
+            <Cycles />
           </div>
           <div className="formRow">
             <button>Enviar</button>
