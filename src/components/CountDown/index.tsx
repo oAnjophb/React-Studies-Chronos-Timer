7d31138af51a58fbf5283a6ba30964891dc4cc77
Ruan Anjos <anjosruanp@gmail.com>
2025-11-06 09:37:39 -0300

    Refactor TaskContext: reorganize context files, update imports, and streamline state management in TaskContextProvider

--- src/components/CountDown/index.tsx
+++ src/components/CountDown/index.tsx
@@ -1,8 +1,8 @@
+import { useTaskContext } from "../../contexts/TaskContext/useTaskContext";
 import styles from "./styles.module.css";
-import { useTaskContext } from "../../contexts/TaskContext";
 
 export function CountDown() {
-  const { state, setState } = useTaskContext();
+  const { state } = useTaskContext();
   return (
     <div className={styles.container}>{state.formattedSecondsRemaining}</div>
   );
